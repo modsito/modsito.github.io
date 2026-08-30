9ede0a1b2f1c6a2054121a297444154758381ecc
modsito <lucakuotazuduarte@gmail.com>
2026-08-29 23:39:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ function mostrarDetalles(nombre, version, peso, novedades, urlV8a, urlV7a, icono
   document.getElementById('det-version').textContent = version;
   document.getElementById('det-peso').textContent = peso;
   document.getElementById('det-novedades').textContent = novedades;
-  document.getElementById('det-icon').src = icono;
   
-  // Asignamos las dos URLs a sus respectivos botones
+  // Asignar icono
+  document.getElementById('det-icon').src = icono;
+
+  // Asignar enlaces directos a los botones
   document.getElementById('btn-descarga-v8a').href = urlV8a;
   document.getElementById('btn-descarga-v7a').href = urlV7a;
 
+  // Mostrar pantalla
   document.getElementById('pantalla-inicio').style.display = 'none';
   document.getElementById('pantalla-detalles').style.display = 'block';
 }
